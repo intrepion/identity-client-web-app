0902cb947b4e3d35b1bbc24f8c3b9e2540f2b418
Oliver Forral <intrepion@gmail.com>
2025-06-14 13:10:17 -0700

    dotnet new mvc --name IdentityClientWebApp

---
+++ IdentityClientWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IdentityClientWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
